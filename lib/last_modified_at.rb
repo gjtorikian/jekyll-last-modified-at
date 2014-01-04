e0f8a6f34ed98dcb246148a3ee534f2ac13e7d69
Garen Torikian <gjtorikian@gmail.com>
2014-01-04 14:25:54 -0800

    Pass along `@format` if such a value exists

--- lib/last_modified_at.rb
+++ lib/last_modified_at.rb
@@ -1,11 +1,16 @@
 module Jekyll
   class LastModifiedAtTag < Liquid::Tag
+    def initialize(tag_name, format, tokens)
+      super
+      @format = format.empty? ? nil : format
+    end
+
     def render(context)
       article_file = context.environments.first["page"]["path"]
       article_file_path = File.join(context.registers[:site].source, article_file)
       last_commit_date = `git log --format="%ct" -- .#{article_file_path}`.strip
       last_modified_time = !last_commit_date.empty? ? last_commit_date : File.mtime(article_file_path)
-      Time.at(last_modified_time.to_i).strftime("%d-%b-%y")
+      Time.at(last_modified_time.to_i).strftime(@format || "%d-%b-%y")
     end
   end
 end
--- spec/plugins/last_modified_at_spec.rb
+++ spec/plugins/last_modified_at_spec.rb
@@ -1,19 +1,20 @@
 require "spec_helper"
 
 describe "Last Modified At Tag" do
-  context "A Post file" do
-    before(:all) do
-      file = "1984-03-06-last-modified-at.md"
-      @post = setup_post(file)
-      do_render(@post, "last_modified_at.html")
-    end
+  context "A committed post file" do
+  def setup(file, layout)
+    @post = setup_post(file)
+    do_render(@post, layout)
+  end
 
     it "has last revised date" do
+      setup("1984-03-06-last-modified-at.md", "last_modified_at.html")
       expect(@post.output).to match /Article last updated on 03-Jan-14/
     end
 
-    it "pass along last revised date format" do
-      expect(@post.output).to match /Article last updated on 03-Jan-14/
+    it "passes along last revised date format" do
+      setup("1984-03-06-last-modified-at-with-format.md", "last_modified_at_with_format.html")
+      expect(@post.output).to match /Article last updated on 2014:January:Saturday:04:09:14:14/
     end
   end
   
