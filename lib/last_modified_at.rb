01844750496db66a347ea6a9241072ea15d7a16f
Parker Moore <parkrmoore@gmail.com>
2014-01-04 14:46:08 -0800

    Fix article_file_path lookup and git invocation

--- lib/last_modified_at.rb
+++ lib/last_modified_at.rb
@@ -2,8 +2,8 @@ module Jekyll
   class LastModifiedAtTag < Liquid::Tag
     def render(context)
       article_file = context.environments.first["page"]["path"]
-      article_file_path = File.join(context.registers[:site].source, article_file)
-      last_commit_date = `git log --format="%ct" -- .#{article_file_path}`.strip
+      article_file_path = File.expand_path(article_file, context.registers[:site].source)
+      last_commit_date = `git log --format="%ct" -- #{article_file_path}`.strip
       last_modified_time = !last_commit_date.empty? ? last_commit_date : File.mtime(article_file_path)
       Time.at(last_modified_time.to_i).strftime("%d-%b-%y")
     end
