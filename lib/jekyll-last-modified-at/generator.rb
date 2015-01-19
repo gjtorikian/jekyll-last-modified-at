5909b8c9e8d938032d82cbc8d07742653e14b7dd
Garen Torikian <gjtorikian@gmail.com>
2015-01-18 21:44:03 -0800

    Iterate over static files, too?

--- lib/jekyll-last-modified-at/generator.rb
+++ lib/jekyll-last-modified-at/generator.rb
@@ -3,7 +3,7 @@ module Jekyll
     class Generator < Jekyll::Generator
 
       def generate(site)
-        %w(posts pages docs_to_write).each do |type|
+        %w(posts pages static_files docs_to_write).each do |type|
           site.send(type).each do |item|
             item.data['last_modified_at'] = Determinator.new(site.source, item.path)
           end
