9b49cf887051cdd44e17efc44110c0a5373845ba
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-12-02 22:21:25 +0000

    Fix #73

--- lib/jekyll-last-modified-at.rb
+++ lib/jekyll-last-modified-at.rb
@@ -2,11 +2,12 @@
 
 module Jekyll
   module LastModifiedAt
+    require 'jekyll-last-modified-at/tag'
+    require 'jekyll-last-modified-at/hook'
+
     autoload :VERSION, 'jekyll-last-modified-at/version'
     autoload :Executor, 'jekyll-last-modified-at/executor'
     autoload :Determinator, 'jekyll-last-modified-at/determinator'
-    autoload :Tag, 'jekyll-last-modified-at/tag'
-    autoload :Hook, 'jekyll-last-modified-at/hook'
     autoload :Git, 'jekyll-last-modified-at/git'
 
     PATH_CACHE = {} # rubocop:disable Style/MutableConstant
